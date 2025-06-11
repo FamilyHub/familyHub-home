25827f9dee24efc42f01c45238a46fc699c9a406
gopiraju <rajumaka911@gmail.com>
2025-06-11 16:08:30 +0530

    Removed un used WebClient from the application

--- src/main/java/com/example/FamilyHub/FamilyHubApplication.java
+++ src/main/java/com/example/FamilyHub/FamilyHubApplication.java
@@ -4,9 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.reactive.config.EnableWebFlux;
-import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
 
 @SpringBootApplication
@@ -16,14 +14,7 @@ import org.springframework.web.reactive.config.WebFluxConfigurer;
 public class FamilyHubApplication implements WebFluxConfigurer {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(FamilyHubApplication.class, args);
-	}
-
-	@Bean
-	public WebClient webClient() {
-
-		return WebClient.builder().build();
 	}
 
 }
